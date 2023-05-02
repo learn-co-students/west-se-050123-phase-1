477ae15b364cf8aaa7abb5527a69eea17cf900b4
betalantz <betalantz@gmail.com>
2023-05-02 12:12:02 -0700

    add comments to 02 starter

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -5,6 +5,19 @@ function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
+//* Create a function that uses a selector to get the header and add the bookStore name as its text content
+// Renders Header
+
+//* Create a function that grabs all the divs form the footer and add the book store name, address, hours and/or phone number
+// Renders Footer
+
+//* 1. use a forEach to iterate over BookStore inventory.
+//  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
+// 3. Add the cardData content to the tags.
+// 4. Append li to the DOM through the ul with the id of book-list
+
+
+
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
@@ -20,4 +33,4 @@ function formatPrice(price) {
   //   <button>Delete</button>
   // </li>
 
-  
+  //* Organize functions calls
