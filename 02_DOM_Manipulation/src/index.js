3562e715d6295e180703e2de7f30c1c649f8d48d
betalantz <betalantz@gmail.com>
2023-05-02 12:09:12 -0700

    add renderHeader and renderFooter

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -5,6 +5,28 @@ function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
+//* Create a function that uses a selector to get the header and add the bookStore name as its text content
+// Renders Header
+function renderHeader(){
+  document.querySelector('#store-name').textContent = bookStore.name
+}
+//* Create a function that grabs all the divs from the footer and add the book store name, address, hours and/or phone number
+// Renders Footer
+
+function renderFooter(){
+  const footerDivs = document.querySelectorAll('footer div')
+  footerDivs[0].textContent = bookStore.name
+  footerDivs[1].textContent = bookStore.number
+  footerDivs[2].textContent = bookStore.address
+}
+
+//* 1. use a forEach to iterate over BookStore inventory.
+//  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
+// 3. Add the cardData content to the tags.
+// 4. Append li to the DOM through the ul with the id of book-list
+
+
+//* Refactor to make the anonymous callback its own function so it can be reused later. 
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
@@ -20,4 +42,6 @@ function formatPrice(price) {
   //   <button>Delete</button>
   // </li>
 
-  
+  //* Organize function calls
+  renderHeader()
+  renderFooter()
